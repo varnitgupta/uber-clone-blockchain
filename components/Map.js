dd2d18b47eead4789203c17cba9a97d8f1828188
varnitgupta <varnit9876@gmail.com>
2022-03-09 18:00:49 +0530

    first commit

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react' 
+import mapboxgl from 'mapbox-gl' 
 
-const Map = () => {
+const style = {
+  wrapper: 'flex-1 h-full w-full'
+} 
+
+console.log(process.env.MAPBOX_ACCESS_TOKEN );
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN 
+
+
+
+const Map = () => { 
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({ 
+      container : 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph', 
+      center : [-99.29011, 39.39172], 
+      zoom : 3, 
+    })
+  },[])
   return (
-    <div>Mapwalaaa</div>
+    <div className={style.wrapper} id = 'map'/>
   )
 }
 
